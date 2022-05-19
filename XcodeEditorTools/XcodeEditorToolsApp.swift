4a4d32ebd0d3c2db5dc411ba12410e6cfbff8531
Alex Moisei <dimentar@gmail.com>
2022-05-19 09:12:05 +0200

    Initial Commit

---
+++ XcodeEditorTools/XcodeEditorToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeEditorToolsApp.swift
+//  XcodeEditorTools
+//
+//  Created by Alexandru Moisei on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeEditorToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
